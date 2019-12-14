02e8598b2def1e36d3d4e54436348051879ec69a
Xavier Davis <xdavis1@icloud.com>
2019-12-14 15:03:43 -0800

    Updates

---
+++ precache-manifest.ba5ea290d1bb41a251e2a629ce9c1879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab894045f6c56148fb0f27a9bf1417de",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "8b38099cd7ff8f604977",
+    "url": "/Clicky-Game/static/css/main.09a192d5.chunk.css"
+  },
+  {
+    "revision": "00d61d0bbab4af0d0cec",
+    "url": "/Clicky-Game/static/js/2.59c7650b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.59c7650b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b38099cd7ff8f604977",
+    "url": "/Clicky-Game/static/js/main.d50d0deb.chunk.js"
+  },
+  {
+    "revision": "e6311db2cb5e070b7b61",
+    "url": "/Clicky-Game/static/js/runtime-main.4cbabcab.js"
+  }
+]);
